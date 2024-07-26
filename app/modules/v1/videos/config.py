be9764fdcbe4e1575e1d9b8b44f09bb701961e84
ellyx13 <pltkiet@gmail.com>
2024-07-26 22:10:22 +0700

    fix(video): set video limit 100mb

--- app/modules/v1/videos/config.py
+++ app/modules/v1/videos/config.py
@@ -2,4 +2,4 @@ CHUNK_SIZE = 5 * 1024 * 1024 # 5 MB
 
 
 ALLOW_FILE_TYPE = ['video/x-msvideo', 'video/mp4', 'video/mpeg', 'video/webm', 'video/avi']
-ALLOW_FILE_SIZE = 52428800 # 50 MB
+ALLOW_FILE_SIZE = 100 * 1024 * 1024 # 100 MB
